2c9955278dfdb589f20641668f6b41055ffcc49e
Yidah <yidahit@hotmail.com>
2020-10-30 11:42:12 +0000

    Added delete-request

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -2,6 +2,7 @@ const listElement = document.querySelector('.posts');
 const postTemplate = document.getElementById('single-post');
 const form = document.querySelector('#new-post form');
 const fetchButton = document.querySelector('#available-posts button');
+const postList = document.querySelector('ul');
 
 function sendHttpRequest(method, url, data) {
   const promise = new Promise((resolve, reject) => {
@@ -48,6 +49,7 @@ async function fetchPosts() {
     const postEl = document.importNode(postTemplate.content, true);
     postEl.querySelector('h2').textContent = post.title.toUpperCase();
     postEl.querySelector('p').textContent = post.body;
+    postEl.querySelector('li').id = post.id;
     listElement.append(postEl);
   }
 }
@@ -64,14 +66,34 @@ async function createPost(title, content) {
   sendHttpRequest('POST', 'https://jsonplaceholder.typicode.com/posts', post);
 }
 
+async function deletePost(title, content) {
+  sendHttpRequest(title, content);
+}
+
+// GET
 fetchButton.addEventListener('click', fetchPosts);
+
+//POST
 form.addEventListener('submit', (event) => {
   event.preventDefault();
   // currentTarget -  It always refers to the element to which the event handler has been attached
   // target - identifies the element on which the event occurred  (add button in this case)
   const enteredTitle = event.currentTarget.querySelector('#title').value;
   const enteredContent = event.currentTarget.querySelector('#content').value;
-  createPost(enteredTitle,enteredContent);
+  createPost(enteredTitle, enteredContent);
 });
 
-
+// DELETE
+postList.addEventListener('click', (event) => {
+  // make sure it was the button that was clicked and not other element in the list
+  if (event.target.tagName === 'BUTTON') {
+    // target is the element that originated the even (in this case delete button)
+    // closest -  traverses the Element and its parents (heading toward the document root)
+    const postId = event.target.closest('li').id;
+    console.log(postId);
+    deletePost(
+      'DELETE',
+      `https://jsonplaceholder.typicode.com/posts/${postId}`
+    );
+  }
+});
